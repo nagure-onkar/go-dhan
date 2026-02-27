c29b42359b7ad3e9601de81588cbd7c051cd2b39
diptiswami2004 <diptiswami8943@gmail.com>
2026-02-27 21:30:38 +0530

    Update endpoints.ts

--- src/api/endpoints.ts
+++ src/api/endpoints.ts
@@ -1,21 +1,24 @@
+export const BASE_URL = "https://astrabytte-ai.onrender.com";
+
 export const ENDPOINTS = {
-  auth: {
-    login: "/api/v1/auth/login",
-  },
-  healthCheck: {
-    root: "",
-  },
-  cattle: {
-    list: "/api/v1/cattle/list",
-    details: (id: string) => `/cattle/${id}`,
-  },
-  milk: {
-    record: "/milk/record",
-    history: "/milk/history",
-  },
-  insemination: {
-    create: "/api/v1/insemination/create",
-    list: "/api/v1/insemination/list",
-    details: (id: string) => `/api/v1/insemination/${id}`,
-  },
+  LOGIN: "/api/v1/auth/login",
+
+  /* ================= VET ================= */
+  CREATE_VET: "/api/v1/vet-registry/",
+  LIST_VET: "/api/v1/vet-registry/",
+  GET_VET: (id: string) => `/api/v1/vet-registry/${id}`,
+  UPDATE_VET: (id: string) => `/api/v1/vet-registry/${id}`,
+  DELETE_VET: (id: string) => `/api/v1/vet-registry/${id}`,
+
+  /* ================= WORKER ================= */
+  ADD_WORKER: "/api/v1/worker-registry/",
+  GET_WORKERS: "/api/v1/worker-registry/",
+  GET_WORKER_BY_ID: (id: string) =>
+    `/api/v1/worker-registry/${id}`,
+  UPDATE_WORKER: (id: string) =>
+    `/api/v1/worker-registry/${id}`,
+  DELETE_WORKER: (id: string) =>
+    `/api/v1/worker-registry/${id}`,
+  UPLOAD_AADHAR: (id: string) =>
+    `/api/v1/worker-registry/${id}/upload-aadhar`,
 };
