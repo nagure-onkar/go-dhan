13170cafec6e75471742abebb5c0b441ddf211e8
diptiswami2004 <diptiswami8943@gmail.com>
2026-02-27 21:27:39 +0530

    Update mr.ts

--- src/constants/localization/languages/mr.ts
+++ src/constants/localization/languages/mr.ts
@@ -5,5 +5,146 @@ export default {
   home: 'मुख्यपृष्ठ',
   lactation: 'दुधकाढणी',
   feed: 'चरापाणी',
-  profit: 'नफा'
+  profit: 'नफा',
+
+//worker
+  "Add New Worker": 'नवीन कामगार जोडा',
+  "Register a new worker with complete details": 'संपूर्ण तपशीलांसह नवीन कामगार नोंदणी करा',
+  "Personal Information": 'वैयक्तिक माहिती',
+  "Contact Information": 'संपर्क माहिती',
+  additionalInfo: 'अतिरिक्त माहिती',
+  workerId: 'कामगार आयडी *',
+  "First Name *" : 'नाव *',
+  "Father's Name *": 'वडिलांचे नाव *',
+  "Surname *": 'आडनाव *',
+  "Gender *": 'लिंग *',
+  male: 'पुरुष',
+  female: 'स्त्री',
+  other: 'इतर',
+  "Date of Birth *": 'जन्मतारीख *',
+  "Age": 'वय',
+  "Salary": 'पगार *',
+  "Date of Joining *": 'रुजू दिनांक *',
+  "Mobile Number *": 'मोबाईल नंबर *',
+  "Alternate Contact Number": 'पर्यायी मोबाईल नंबर',
+  "Address *": 'पत्ता *',
+  "Additional Information":'अतिरिक्त माहिती',
+  "Remarks": 'टीप',
+  "Document Upload": 'दस्तऐवज अपलोड',
+  "Upload Aadhar Card Image": 'आधार कार्ड प्रत अपलोड करा',
+  "Cancel": 'रद्द करा',
+  "Save Worker": 'कामगार जतन करा',
+  "Saving": 'जतन करत आहे...',
+  success: 'यशस्वी',
+  workerAdded: 'कामगार यशस्वीरित्या जोडला',
+
+
+//vet
+
+  "Add New Veterinarian": "नवीन पशुवैद्य जोडा",
+  "Register a new veterinarian with complete details":"संपूर्ण तपशीलांसह नवीन पशुवैद्य नोंदणी करा",
+  "Professional Information": "व्यावसायिक माहिती",
+  "Veterinarian ID*": "पशुवैद्य आयडी*",
+  "e.g., VET-001": "उदा. VET-001",
+
+  "First Name*": "नाव*",
+  "e.g., Dr. Meena": "उदा. डॉ. मीना",
+
+  "Father's Name*": "वडिलांचे नाव*",
+  "e.g., Suresh": "उदा. सुरेश",
+
+  "Surname*": "आडनाव*",
+  "e.g., Patil": "उदा. पाटील",
+
+  "Date of Birth*": "जन्मतारीख*",
+  "mm/dd/yyyy": "महिना/दिनांक/वर्ष",
+
+  
+  "Auto-calculated": "स्वयंचलित गणना",
+
+  "Date of Joining*": "रुजू दिनांक*",
+
+  "Mobile Number*": "मोबाईल क्रमांक*",
+  "9876543211": "९८७६५४३२११",
+
+
+  "Optional": "ऐच्छिक",
+
+  "Address*": "पत्ता*","e.g., Pune District Hospital, Pune":"उदा. पुणे जिल्हा रुग्णालय, पुणे",
+
+  "Specialization & Remarks": "विशेषज्ञता आणि टीप",
+  "e.g., Specialist in bovine health, Large animal surgery expertise...":
+    "उदा. गोवंश आरोग्य तज्ञ, मोठ्या प्राण्यांच्या शस्त्रक्रियेत तज्ञ...",
+    
+  "Save Veterinarian": "पशुवैद्य जतन करा",
+
+
+//call dr
+    
+  "Call Doctor": "डॉक्टरांना बोलवा",
+  "Schedule a veterinary visit for your cattle": "आपल्या जनावरांसाठी पशुवैद्यकीय भेट निश्चित करा",
+  "Basic Details": "मूलभूत माहिती",
+  "Cattle ID": "जनावर आयडी",
+  "Enter cattle ID": "जनावर आयडी टाका",
+  "Date & Time": "दिनांक आणि वेळ",
+  "Visit Details": "भेटीचा तपशील",
+  "Doctor Visit Confirmed": "डॉक्टरांची भेट निश्चित झाली आहे का",
+  "Select": "निवडा",
+  "Yes": "हो",
+  "No": "नाही",
+  "Reason to Call Doctor": "डॉक्टरांना बोलवण्याचे कारण",
+  "Select Reason": "कारण निवडा",
+  "Heat Confirmation": "हीट तपासणी",
+  "Treatment": "उपचार",
+  "Insemination / Delivery Details": "गर्भधारणा / प्रसूती तपशील",
+  "Treatment Follow Up": "उपचारानंतरची तपासणी",
+  
+  
+  "Any additional notes or observations...": "काही अतिरिक्त नोंदी किंवा निरीक्षणे...",
+  
+  "Schedule Visit": "भेट निश्चित करा",
+  //assessment
+
+  "Assessment Details": "मूल्यमापन",
+  "Assessment Date": "मूल्यमापन तारीख",
+  //"Select Date": "तारीख निवडा",
+  "Backbone & Ribs": "पाठीचा कणा आणि बरगड्या",
+
+  
+  "Select Cattle": "जनावर निवडा",
+  "Current State": "सध्याची स्थिती",
+  "Healthy": "निरोगी",
+  "Sick": "आजारी",
+  "Under Treatment": "उपचार सुरू",
+  
+  "Body Length & Weight": "शरीराची लांबी आणि वजन",
+  "Weight": "वजन",
+  "Hip Height": "कंबरेची उंची",
+  "Body Length": "शरीराची लांबी",
+  "Heart Girth": "छातीचा घेर",
+
+
+  "Assessed By": "मूल्यमापन केले",
+  "Owner": "मालक",
+  "Doctor": "डॉक्टर",
+  "BCS Score":"BCS गुण",
+
+  "Body Condition Score (BCS)": "शरीर स्थिती गुण (BCS)",
+  "Select BCS": "BCS निवडा",
+
+
+  "Expenses": "खर्च",
+  "Doctor Fees": "डॉक्टर फी",
+  "Treatment Expenses": "उपचार खर्च",
+  "Other Expenses": "इतर खर्च",
+  "Total Expenses": "एकूण खर्च",
+
+  "Add Remarks": "नोंदी जोडा",
+
+  "Save Assessment": "मूल्यमापन जतन करा",
+  "Update Assessment": "मूल्यमापन अपडेट करा",
+ 
+
+
+ 
 };
