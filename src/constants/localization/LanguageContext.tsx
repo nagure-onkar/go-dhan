58783feae5107a5f513ceafe43f0d250b74e6f31
diptiswami2004 <diptiswami8943@gmail.com>
2026-02-28 10:05:00 +0530

    Update LanguageContext.tsx

--- src/constants/localization/LanguageContext.tsx
+++ src/constants/localization/LanguageContext.tsx
@@ -3,29 +3,29 @@ import { languages, LanguageKey } from '@/constants/localization';
 
 interface LanguageContextType {
   language: LanguageKey;
-  t: (key: string) => string;
+  t: typeof languages["en"];
   setLanguage: (lang: LanguageKey) => void;
 }
 
 
 export const LanguageContext = createContext<LanguageContextType>({
   language: 'en',
-  t: (key: string) => key, 
+  t: languages.en,
   setLanguage: () => {},
 });
 
 export default function LanguageProvider({ children }: { children: ReactNode }) {
   const [language, setLanguage] = useState<LanguageKey>('en');
+    const t: typeof languages["en"] = languages[language] as typeof languages["en"];
 
-  
-  const t = (key: string) => languages[language][key] || key;
+
 
   return (
     <LanguageContext.Provider value={{ language, t, setLanguage }}>
       {children}
     </LanguageContext.Provider>
   );
-}
+};
 
 
 export const useLanguage = () => useContext(LanguageContext);
